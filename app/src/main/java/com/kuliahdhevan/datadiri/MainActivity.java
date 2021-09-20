9af55b61683556a184bb22beb51b4a5d4d9e0369
Dhevan Muhamad <dhevanmuhamad@gmail.com>
2021-09-20 23:35:01 +0700

    tambah tombol buka cv

--- app/src/main/java/com/kuliahdhevan/datadiri/MainActivity.java
+++ app/src/main/java/com/kuliahdhevan/datadiri/MainActivity.java
@@ -2,7 +2,11 @@ package com.kuliahdhevan.datadiri;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void openResume(View view) {
+        String url = "https://dhevanthareza.github.io/resume_dhevan.pdf";
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("Implicit_Intent", "Intent Error");
+        }
     }
 }
